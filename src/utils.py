467f0b7340f7221b79a99c1dfde1cc0816e46ed7
Адушинов Владимир <Adushinov13@gmail.com>
2025-07-14 14:48:14 +0800

    выполнена домашняя работа урока 12.1

---
+++ src/utils.py
@@ -0,0 +1,19 @@
+import os
+import json
+
+
+def load_transactions(file_path):
+    if not os.path.exists(file_path):
+        return []
+
+    try:
+        with open(file_path, 'r', encoding='utf-8') as f:
+            transactions = json.load(f)
+
+        if isinstance(transactions, list):
+            return transactions
+        else:
+            return []
+
+    except json.JSONDecodeError:
+        return []
