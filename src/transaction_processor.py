6ef961277b17a37db76d64967a0b1b507e0e3a26
Адушинов Владимир <Adushinov13@gmail.com>
2025-07-16 02:34:59 +0800

    fixed homework 12.1

--- src/transaction_processor.py
+++ src/transaction_processor.py
@@ -1,11 +1,27 @@
-from external_api.currency_converter import CurrencyConverter
+import os
+import requests
 
+def currency_converter(amount: float, currency_from: str, currency_to: str = "RUB") -> float:
+    url = "https://api.apilayer.com/exchangerates_data/convert"
+    headers = {"apikey": os.getenv("EXCHANGE_API_KEY")}
+    params = {"amount": amount, "from": currency_from, "to": currency_to}
+
+    response = requests.get(url, headers=headers, params=params).json()
+    return float(response.get("result", 0))
 
 def process_transaction(transaction: dict) -> float:
-    amount = transaction.get('amount')
-    currency = transaction.get('currency')
+    """
+    Обрабатывает транзакцию и возвращает сумму в рублях
+    :param transaction: словарь с полями 'amount' и 'currency'
+    :return: сумма в рублях (float)
+    """
+    amount = transaction.get("operationAmount", {}).get("amount")
+    currency = transaction.get("operationAmount", {}).get("currency", {}).get("code")
 
     if not amount or not currency:
-        raise ValueError("Transaction must contain 'amount' and 'currency' fields")
+        raise ValueError("Транзакция должна содержать поля 'amount' и 'currency'")
 
-    return CurrencyConverter.convert_to_rub(amount, currency)
+    if currency == "RUB":
+        return float(amount)
+    else:
+        return currency_converter(amount, currency)
